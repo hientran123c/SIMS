1c1c37038006b91b3fd8bedaf9b3a40095df8899
Kiên <knight.45.acgg@gmail.com>
2025-03-28 12:56:12 +0700

    Build, debug run and Syn envi

--- UserManagement/obj/Debug/net8.0/UserManagement.AssemblyInfo.cs
+++ UserManagement/obj/Debug/net8.0/UserManagement.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UserManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+711ba16b81260ec449a023eb75db98d9fcf10dae")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a53468a53a01e72259b0082f2b1ae8a4eb7672c9")]
 [assembly: System.Reflection.AssemblyProductAttribute("UserManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UserManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
